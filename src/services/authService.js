ea928e04c33c4531abe670d6da0597257f896dd3
Nguyen Tuan Kiet <ntk28042002@gmail.com>
2024-09-17 23:04:04 +0700

    create services for handle api

--- src/services/authService.js
+++ src/services/authService.js
@@ -1,7 +1,8 @@
-import axiosInstance from "@/utils/axiosInstance";
+import { API } from '@/api/apiUrl';
+import axiosInstance from '@/utils/axiosInstance';
 
 export const authService = {
   login() {
-    return axiosInstance.post(`/login`);
+    return axiosInstance.post(`${API.LOGIN}`);
   },
 };
