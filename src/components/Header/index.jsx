1852ce5c655960de19a1eaf78172e33d074d7640
Nguyen Tuan Kiet <ntk28042002@gmail.com>
2024-10-18 20:48:20 +0700

    fix: header menu

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,44 +1,26 @@
 import { PATH } from "@/constant/path";
 import { generateCartoonAvatar } from "@/utils/avatarUtils";
-import { useTheme } from "@emotion/react";
-import { Menu as MenuIcon } from "@mui/icons-material";
 import AccountCircleIcon from "@mui/icons-material/AccountCircle"; // Profile icon
-import ChevronLeftIcon from "@mui/icons-material/ChevronLeft";
-import ChevronRightIcon from "@mui/icons-material/ChevronRight";
 import LogoutIcon from "@mui/icons-material/Logout"; // Logout icon
 import {
   AppBar,
   Avatar,
-  Divider,
-  Drawer,
   IconButton,
   Menu,
   MenuItem,
-  styled,
   Toolbar,
   Typography,
 } from "@mui/material";
 import { useState } from "react";
 import { useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
-import MenuList from "../Menu";
 
 const drawerWidth = 240;
 
-const Header = () => {
-  const theme = useTheme();
-  const [open, setOpen] = useState(false);
+const Header = ({ handleDrawerToggle }) => {
   const { profile } = useSelector((state) => state.profile);
   const [anchorEl, setAnchorEl] = useState(null);
   const navigate = useNavigate();
-
-  const handleDrawerOpen = () => {
-    setOpen(true);
-  };
-
-  const handleDrawerClose = () => {
-    setOpen(false);
-  };
 
   const handleMenuClick = (event) => {
     setAnchorEl(event.currentTarget);
@@ -61,25 +43,32 @@ const Header = () => {
 
   return (
     <>
-      <MUIAppBar position="fixed" open={open}>
-        <Toolbar sx={{ height: "75px" }}>
+      <AppBar
+        position="fixed"
+        sx={{
+          width: { sm: `calc(100% - ${drawerWidth}px)` },
+          ml: { sm: `${drawerWidth}px` },
+        }}
+      >
+        <Toolbar
+          sx={{
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "space-between",
+          }}
+        >
           <IconButton
             color="inherit"
             aria-label="open drawer"
-            onClick={handleDrawerOpen}
             edge="start"
-            sx={{
-              marginRight: 5,
-              ...(open && { display: "none" }),
-            }}
+            onClick={handleDrawerToggle}
+            sx={{ mr: 2, display: { sm: "none" } }}
           >
-            {/* Increase the size of the MenuIcon */}
-            <MenuIcon sx={{ fontSize: "2.5rem", marginLeft: "6px" }} />
+            <Menu />
           </IconButton>
-          <Typography variant="h4" noWrap component="div" sx={{ flexGrow: 1 }}>
-            Mini variant drawer
+          <Typography variant="h6" noWrap component="div">
+            Responsive drawer
           </Typography>
-
           {/* User Avatar/Button to Open Menu */}
           <IconButton color="inherit" onClick={handleMenuClick}>
             <Avatar
@@ -92,129 +81,23 @@ const Header = () => {
               }}
             />
           </IconButton>
-
           {/* User Menu */}
-          <StyledMenu
+          <Menu
             anchorEl={anchorEl}
             open={Boolean(anchorEl)}
             onClose={handleMenuClose}
           >
-            <StyledMenuItem onClick={handleProfile}>
+            <MenuItem onClick={handleProfile}>
               <AccountCircleIcon sx={{ mr: 1.5, fontSize: "2rem" }} /> Profile
-            </StyledMenuItem>
-            <StyledMenuItem onClick={handleLogout}>
+            </MenuItem>
+            <MenuItem onClick={handleLogout}>
               <LogoutIcon sx={{ mr: 1.5, fontSize: "2rem" }} /> Logout
-            </StyledMenuItem>
-          </StyledMenu>
+            </MenuItem>
+          </Menu>
         </Toolbar>
-      </MUIAppBar>
-
-      <MuiDrawer variant="permanent" open={open}>
-        <DrawerHeader>
-          <IconButton onClick={handleDrawerClose}>
-            {theme.direction === "rtl" ? (
-              <ChevronRightIcon />
-            ) : (
-              <ChevronLeftIcon />
-            )}
-          </IconButton>
-        </DrawerHeader>
-        <Divider />
-        <MenuList />
-        <Divider />
-      </MuiDrawer>
+      </AppBar>
     </>
   );
 };
 
 export default Header;
-
-// Styled Menu to make it larger and more visually appealing
-const StyledMenu = styled(Menu)(({ theme }) => ({
-  "& .MuiPaper-root": {
-    borderRadius: "10px",
-    padding: theme.spacing(1),
-    backgroundColor: "#f0f0f0",
-    boxShadow: "0px 4px 10px rgba(0, 0, 0, 0.1)",
-    minWidth: "150px",
-  },
-}));
-
-// Styled MenuItem
-const StyledMenuItem = styled(MenuItem)(({ theme }) => ({
-  fontSize: "1.5rem",
-  padding: theme.spacing(1),
-  paddingLeft: "10px",
-  display: "flex",
-  alignItems: "center",
-  "&:hover": {
-    backgroundColor: theme.palette.primary.main,
-    color: "#fff",
-  },
-}));
-
-// MUI Styled Components
-const MUIAppBar = styled(AppBar, {
-  shouldForwardProp: (prop) => prop !== "open",
-})(({ theme, open }) => ({
-  zIndex: theme.zIndex.drawer + 1,
-  transition: theme.transitions.create(["width", "margin"], {
-    easing: theme.transitions.easing.sharp,
-    duration: theme.transitions.duration.leavingScreen,
-  }),
-  ...(open && {
-    marginLeft: drawerWidth,
-    width: `calc(100% - ${drawerWidth}px)`,
-    transition: theme.transitions.create(["width", "margin"], {
-      easing: theme.transitions.easing.sharp,
-      duration: theme.transitions.duration.enteringScreen,
-    }),
-  }),
-}));
-
-const openedMixin = (theme) => ({
-  width: drawerWidth,
-  transition: theme.transitions.create("width", {
-    easing: theme.transitions.easing.sharp,
-    duration: theme.transitions.duration.enteringScreen,
-  }),
-  overflowX: "hidden",
-});
-
-const closedMixin = (theme) => ({
-  transition: theme.transitions.create("width", {
-    easing: theme.transitions.easing.sharp,
-    duration: theme.transitions.duration.leavingScreen,
-  }),
-  overflowX: "hidden",
-  width: `calc(${theme.spacing(7)} + 15px)`,
-  [theme.breakpoints.up("sm")]: {
-    width: `calc(${theme.spacing(8)} + 15px)`,
-  },
-});
-
-const DrawerHeader = styled("div")(({ theme }) => ({
-  display: "flex",
-  alignItems: "center",
-  justifyContent: "flex-end",
-  padding: theme.spacing(0, 1),
-  height: "80px",
-  ...theme.mixins.toolbar,
-}));
-
-const MuiDrawer = styled(Drawer, {
-  shouldForwardProp: (prop) => prop !== "open",
-})(({ theme, open }) => ({
-  width: drawerWidth,
-  flexShrink: 0,
-  whiteSpace: "nowrap",
-  boxSizing: "border-box",
-  ...(open && {
-    ...openedMixin(theme),
-    "& .MuiDrawer-paper": openedMixin(theme),
-  }),
-  ...(!open && {
-    ...closedMixin(theme),
-    "& .MuiDrawer-paper": closedMixin(theme),
-  }),
-}));
--- src/layout/MainLayout.jsx
+++ src/layout/MainLayout.jsx
@@ -1,13 +1,10 @@
+import Header from "@/components/Header";
 import MenuList from "@/components/Menu";
-import MenuIcon from "@mui/icons-material/Menu";
-import AppBar from "@mui/material/AppBar";
 import Box from "@mui/material/Box";
 import CssBaseline from "@mui/material/CssBaseline";
 import Divider from "@mui/material/Divider";
 import Drawer from "@mui/material/Drawer";
-import IconButton from "@mui/material/IconButton";
 import Toolbar from "@mui/material/Toolbar";
-import Typography from "@mui/material/Typography";
 import PropTypes from "prop-types";
 import * as React from "react";
 import { Outlet } from "react-router-dom";
@@ -49,28 +46,7 @@ function MainLayout(props) {
   return (
     <Box sx={{ display: "flex" }}>
       <CssBaseline />
-      <AppBar
-        position="fixed"
-        sx={{
-          width: { sm: `calc(100% - ${drawerWidth}px)` },
-          ml: { sm: `${drawerWidth}px` },
-        }}
-      >
-        <Toolbar>
-          <IconButton
-            color="inherit"
-            aria-label="open drawer"
-            edge="start"
-            onClick={handleDrawerToggle}
-            sx={{ mr: 2, display: { sm: "none" } }}
-          >
-            <MenuIcon />
-          </IconButton>
-          <Typography variant="h6" noWrap component="div">
-            Responsive drawer
-          </Typography>
-        </Toolbar>
-      </AppBar>
+      <Header handleDrawerToggle={handleDrawerToggle} />
       <Box
         component="nav"
         sx={{ width: { sm: drawerWidth }, flexShrink: { sm: 0 } }}
